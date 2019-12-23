d44aa5a31f70dd5bff1ce1b106e242352004c4ba
Vaastav Anand <vaastav.anand05@gmail.com>
2019-12-23 08:36:26 -0800

    Add MySQL integration

--- tracy.py
+++ tracy.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import json
 import sys
+from mysql.connector import Error, connect
 
 api = Flask(__name__)
+connection = {}
 
 @api.route("/summary", methods=['GET'])
 def summary():
@@ -11,13 +13,24 @@ def summary():
     return json.dumps({"error": "Not Implemented"})
 
 def main():
+    global connection
     if len(sys.argv) != 2:
         print("Usage: python tracy.py <config_file>")
         sys.exit(1)
     config_file = sys.argv[1]
     with open(config_file, 'r+') as inf:
         config = json.load(inf)
-        print(config)
+        db_host = config["db_host"]
+        database = config["db"]
+        user = config["username"]
+        password = config["pwd"]
+        try:
+            connection = connect(host=db_host, database=database, user=user, password=password)
+            if connection.is_connected():
+                print("Connected to MySQL server")
+        except Error as e:
+            print("Error while connecting to database", e)
+            sys.exit(1)
     api.run(host=config["host"], port=config["port"])
 
 if __name__ == '__main__':
